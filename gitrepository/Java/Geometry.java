270b9e8496f57f299e7445eca5ca661c140173d4
Tanavi <tanaviprakashborhade.com>
2023-08-17 21:48:31 +0530

    java file added

---
+++ gitrepository/Java/Geometry.java
@@ -0,0 +1,16 @@
+class Shape {
+    String color;
+
+}
+class Triangle extends Shape {
+
+}
+
+
+public class Geometry {
+
+    public static void main(String args[]) {
+    Triangle t1 = new Triangle();
+    t1.color = "red";
+    }
+}
